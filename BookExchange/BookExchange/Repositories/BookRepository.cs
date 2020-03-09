a154268337b093393e7586402185e49e58a1d9fc
nicmol <38054536+nicmol@users.noreply.github.com>
2020-03-09 11:20:00 -0700

    admin pages hooked up, button designs

--- BookExchange/BookExchange/Repositories/BookRepository.cs
+++ BookExchange/BookExchange/Repositories/BookRepository.cs
@@ -10,54 +10,54 @@ namespace BookExchange.Repositories
 {
     public class BookRepository
     {
-    //    public BookRepository(ApplicationDbContext context)
-    //    {
-    //        _context = context;
-    //    }
-    //    private readonly ApplicationDbContext _context;
+        public BookRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        private readonly ApplicationDbContext _context;
 
-    //    public List<Book> Books
-    //    {
-    //        get
-    //        {
-    //            return _context.Books.Include(b => b.appUser);
-    //        }
-    //    }
+        //public ICollection<Book> Books
+        //{
+        //    get
+        //    {
+        //        return _context.Books.Include(b => b.appUser);
+        //    }
+        //}
 
-    //    public void AddBook(Book Book)
-    //    {
-    //        _context.Books.Add(Book);
-    //        _context.SaveChanges();
-    //    }
+        public void AddBook(Book Book)
+        {
+            _context.Books.Add(Book);
+            _context.SaveChanges();
+        }
 
-    //    public void AddReplyToBook(AppUser user, string BookText, int BookId)
-    //    {
+        //    public void AddReplyToBook(AppUser user, string BookText, int BookId)
+        //    {
 
-    //        Reply reply = new Reply();
-    //        reply.BookText = BookText;
-    //        reply.Sender = user;
-    //        reply.Date = DateTime.Now;
+        //        Reply reply = new Reply();
+        //        reply.BookText = BookText;
+        //        reply.Sender = user;
+        //        reply.Date = DateTime.Now;
 
-    //        _context.Books.Find(BookId).Replies.Add(reply);
+        //        _context.Books.Find(BookId).Replies.Add(reply);
 
-    //        _context.SaveChanges();
-    //    }
+        //        _context.SaveChanges();
+        //    }
 
-    //    public Book getBookByDate(DateTime date)
-    //    {
-    //        Book Book = Books.FirstOrDefault(m => m.Date.ToString() == date.ToString());
-    //        return Book;
-    //    }
+        //    public Book getBookByDate(DateTime date)
+        //    {
+        //        Book Book = Books.FirstOrDefault(m => m.Date.ToString() == date.ToString());
+        //        return Book;
+        //    }
 
-    //    public Book getBookById(int BookId)
-    //    {
-    //        Book Book = Books.Find(m => m.BookId == BookId);
-    //        return Book;
-    //    }
+        //    public Book getBookById(int BookId)
+        //    {
+        //        Book Book = Books.Find(m => m.BookId == BookId);
+        //        return Book;
+        //    }
 
-    //    public void AddReplyToBook(string firstName, string lastName, string email, string BookText, string BookDate)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
+        //    public void AddReplyToBook(string firstName, string lastName, string email, string BookText, string BookDate)
+        //    {
+        //        throw new NotImplementedException();
+        //    }
     }
 }
